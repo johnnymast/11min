d2f305b1c0c37a3ffce6eead899521d4fc8d0ae1
Johnny Mast <mastjohnny@gmail.com>
2017-02-11 11:51:30 +0100

    Testing css/js in production mode

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ require('laravel-elixir-vue-2');
 
 elixir(mix => {
     mix.sass('app.scss')
-        .webpack('app.js')
+        .webpack('app.js', 'all.js')
         .scripts([
             'clipboard.min.js',
             'general.js',
