b460ebda79a56fffeb78240b083246504dfac568
Johnny Mast <mastjohnny@gmail.com>
2017-03-04 13:38:46 +0100

    Renamed usertable migration

--- database/migrations/2017_03_04_132347_update_users_add_remember_token.php
+++ database/migrations/2017_03_04_132347_update_users_add_remember_token.php
@@ -13,7 +13,7 @@ class CreateUsersTableAddRememberToken extends Migration
      */
     public function up()
     {
-        Schema::table('Users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->rememberToken();
         });
     }
@@ -25,7 +25,7 @@ class CreateUsersTableAddRememberToken extends Migration
      */
     public function down()
     {
-        Schema::table('Users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             //
         });
     }
