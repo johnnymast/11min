7780acebda2edcabb09e7f9a71f17ef7932dc844
Johnny Mast <mastjohnny@gmail.com>
2017-03-04 13:20:01 +0100

    Added a logout link

--- resources/views/snippets/nav.blade.php
+++ resources/views/snippets/nav.blade.php
@@ -31,7 +31,7 @@
             <a class="nav-link" href="{{ route('logout') }}"
                onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
+                {{ 'Logout' }}
             </a>
 
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
